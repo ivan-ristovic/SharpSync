e3b760d6145303fb26796861e8d63f4d4cd8c183
Ivan Ristovic <ivo.ristovic@gmail.com>
2020-01-04 00:55:06 +0100

    [Project] Init

---
+++ SharpSync/SharpSync/Common/Options.cs
@@ -0,0 +1,36 @@
+﻿using CommandLine;
+
+namespace SharpSync.Common
+{
+    [Verb("add", HelpText = "Add a new sync rule.")]
+    internal sealed class AddOptions
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        [Value(0, Required = true, HelpText = "Source path.")]
+        public string? Source { get; set; }
+
+        [Value(1, Required = true, HelpText = "Destination path.")]
+        public string? Destination { get; set; }
+
+        [Option('z', "zip", Required = false, HelpText = "Create archive instead of regular copy.")]
+        public bool ShouldZip { get; set; }
+    }
+
+    [Verb("remove", HelpText = "Remove existing sync rule.")]
+    internal sealed class RemoveOptions
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        [Value(0, Required = true, HelpText = "Index of rule to remove.")]
+        public int? Index { get; set; }
+    }
+
+    [Verb("list", HelpText = "List all registered sync rules.")]
+    internal sealed class ListOptions
+    {
+
+    }
+}
---
+++ SharpSync/SharpSync/Common/Setup.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+namespace SharpSync.Common
+{
+    public static class Setup
+    {
+        public static void Logger(bool verbose)
+        {
+            LoggerConfiguration lcfg = new LoggerConfiguration()
+                .WriteTo.Console(outputTemplate: "\r[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                ;
+
+            if (verbose)
+                lcfg.MinimumLevel.Verbose();
+            else
+                lcfg.MinimumLevel.Information();
+
+            Log.Logger = lcfg.CreateLogger();
+        }
+    }
+}
